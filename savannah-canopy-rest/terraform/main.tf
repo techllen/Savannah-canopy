979345f107871d7cfd68103af1e4f8642a60611c
techllen <andre2119@yahoo.com>
2025-03-06 07:56:58 -0500

    infra

--- savannah-canopy-rest/terraform/main.tf
+++ savannah-canopy-rest/terraform/main.tf
@@ -71,10 +71,35 @@ EOF
 # IAM Policies for CodePipeline Role
 # ---------------------------------------------------------------------------------------------------------------------
 
+# Add an inline policy to the codebuild_role for S3 access
+resource "aws_iam_role_policy" "codebuild_s3_policy" {
+  name = "codebuild-s3-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::plantstore-codepipeline-artifacts/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
 # Add an inline policy to the codepipeline_role for S3 access
 resource "aws_iam_role_policy" "codepipeline_s3_policy" {
   name = "codepipeline-s3-policy"
-  role = aws_iam_role.codebuild_role.id
+  role = aws_iam_role.codepipeline_role.id
 
   policy = <<EOF
 {
