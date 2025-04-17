6f6f191da6db8e7404cf7067e8c910f3f5cfa48e
Allen M. <60898418+techllen@users.noreply.github.com>
2025-04-16 23:59:57 -0400

    Fixing NullPointerException in DiscountService.calculateDiscount()

--- savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
+++ savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
@@ -1,20 +1,23 @@
 package com.plants_store.savannah_canopy.service;
 
-import com.plants_store.savannah_canopy.exception.ErrorContext;
-import com.plants_store.savannah_canopy.exception.PlantNotFoundException;
-
 /**
  * Interface defining operations for applying discounts.
  */
 public interface DiscountService {
 
-    /**
-     * Calculates the discounted price for a given plant and percentage.
-     * @param plantId The ID of the plant.
-     * @param percentage The discount percentage to apply.
-     * @return The calculated price after applying the discount.
-     * @throws PlantNotFoundException if the plant is not found.
-     * @throws ErrorContext if an error occurs during calculation.
-     */
-    double calculateDiscount(Long plantId, int percentage) throws PlantNotFoundException, ErrorContext;
-}
+ /**
+ * Calculates the discounted price for a given plant and percentage.
+ * @param plantId The ID of the plant. Can't be null.
+ * @param percentage The discount percentage to apply.
+ * @return The calculated price after applying the discount.
+ * @throws com.plants_store.savannah_canopy.exception.ErrorContext if the plant is not found or an error occurs during calculation.
+ */
+ double calculateDiscount(Long plantId, int percentage);
+
+ // Added null check
+ double calculateDiscount(Long plantId, int percentage) {
+ if (plantId == null) {
+ throw new ErrorContext("Plant ID can't be null.");
+ }
+ // Rest of the code
+ }
