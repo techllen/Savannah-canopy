1f958acf46386b22da07cf48be04bcdb791cfb15
Allen M. <60898418+techllen@users.noreply.github.com>
2025-04-16 22:04:51 -0400

    Fixing NPE caused by null DiscountService instance

--- savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
+++ savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
@@ -1,16 +1,19 @@
-package com.plants_store.savannah_canopy.service;
-
-/**
- * Interface defining operations for applying discounts.
- */
+// Added null check
 public interface DiscountService {
 
     /**
-     * Calculates the discounted price for a given plant and percentage.
+     * Calculates the discounted price for a given plant and percentage. 
      * @param plantId The ID of the plant.
      * @param percentage The discount percentage to apply.
      * @return The calculated price after applying the discount.
      * @throws com.plants_store.savannah_canopy.exception.ErrorContext if the plant is not found or an error occurs during calculation.
      */
-    double calculateDiscount(Long plantId, int percentage);
+    double calculateDiscount(Long plantId, int percentage) {
+        if (this instanceof DiscountServiceImpl) {
+            DiscountServiceImpl discountServiceImpl = (DiscountServiceImpl) this;
+            return discountServiceImpl.calculateDiscount(plantId, percentage);
+        } else {
+            throw new NullPointerException("DiscountService instance is null.");
+        }
+    }
 }
