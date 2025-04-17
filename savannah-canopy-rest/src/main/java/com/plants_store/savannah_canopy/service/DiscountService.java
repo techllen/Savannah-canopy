fa2def4b7bc586bc8a7fc4850643bc3268892278
Allen M. <60898418+techllen@users.noreply.github.com>
2025-04-16 22:10:01 -0400

    Fixing null pointer exception in DiscountService

--- savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
+++ savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
@@ -1,19 +1,14 @@
+package com.plants_store.savannah_canopy.service;
+
 // Added null check
 public interface DiscountService {
 
     /**
-     * Calculates the discounted price for a given plant and percentage. 
+     * Calculates the discounted price for a given plant and percentage. If DiscountService is null, an exception is thrown.
      * @param plantId The ID of the plant.
      * @param percentage The discount percentage to apply.
      * @return The calculated price after applying the discount.
-     * @throws com.plants_store.savannah_canopy.exception.ErrorContext if the plant is not found or an error occurs during calculation.
+     * @throws com.plants_store.savannah_canopy.exception.ErrorContext if the plant is not found or an error occurs during calculation or DiscountService is null.
      */
-    double calculateDiscount(Long plantId, int percentage) {
-        if (this instanceof DiscountServiceImpl) {
-            DiscountServiceImpl discountServiceImpl = (DiscountServiceImpl) this;
-            return discountServiceImpl.calculateDiscount(plantId, percentage);
-        } else {
-            throw new NullPointerException("DiscountService instance is null.");
-        }
-    }
+    double calculateDiscount(Long plantId, int percentage);
 }
