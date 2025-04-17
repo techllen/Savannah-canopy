77b46438c2896177bba585378c0a52aefdf067db
Allen M. <60898418+techllen@users.noreply.github.com>
2025-04-16 22:54:06 -0400

    Fixing NullPointerException by adding null check for plantId in calculateDiscount method

--- savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
+++ savannah-canopy-rest/src/main/java/com/plants_store/savannah_canopy/service/DiscountService.java
@@ -7,17 +7,14 @@ public interface DiscountService {
 
     /**
      * Calculates the discounted price for a given plant and percentage.
-     * @param plantId The ID of the plant. Can't be null.
+     * @param plantId The ID of the plant.
      * @param percentage The discount percentage to apply.
      * @return The calculated price after applying the discount.
      * @throws com.plants_store.savannah_canopy.exception.ErrorContext if the plant is not found or an error occurs during calculation.
      */
-    double calculateDiscount(Long plantId, int percentage);
-
-    // Added null check
     double calculateDiscount(Long plantId, int percentage) {
         if (plantId == null) {
-            throw new ErrorContext("Plant ID can't be null");
+            throw new ErrorContext("Plant ID is null");
         }
         // Calculate discount
     }
